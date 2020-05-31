728e5aa8841585b9fe926dd506c02ffa0fed4206
Muhamad Nadim <nadimshafii@gmail.com>
2020-05-31 21:41:27 +0800

    Initial commit for Assignment Lab1's Group 25 (Muhamad Nadim and Amirul Azri)

---
+++ src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
@@ -0,0 +1,14 @@
+/*
+In a group of two members, design and develop a Java program that performs simple calculation.
+Your program will read in values and operation to be calculated from the user and display the result.
+Your program is able to continue the calculation using the result from previous calculation plus a new value and new operation given by the user.
+Allow the user the select the operation of addition, subtraction, and multiplication, division, power (x^y), and square (x^2).
+User can reset the value back to zero just like normal calculator. Loop the operations until user chooses to quit the program.
+ */
+package Assignment;
+
+public class Lab1Y3G25_MuhamadNadim_AmirulAzri {
+    public static void main(String[] args) {
+     System.out.println("Muhamad Nadim");
+    }
+}
