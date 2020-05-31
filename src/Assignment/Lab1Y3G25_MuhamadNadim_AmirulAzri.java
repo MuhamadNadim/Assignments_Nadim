dc852466e87fcb8595eef8785b0915d497f0f6d2
Muhamad Nadim <nadimshafii@gmail.com>
2020-05-31 21:45:13 +0800

    Added Git & Github Tutorials on the assignment Java file.

--- src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
+++ src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
@@ -4,6 +4,9 @@ Your program will read in values and operation to be calculated from the user an
 Your program is able to continue the calculation using the result from previous calculation plus a new value and new operation given by the user.
 Allow the user the select the operation of addition, subtraction, and multiplication, division, power (x^y), and square (x^2).
 User can reset the value back to zero just like normal calculator. Loop the operations until user chooses to quit the program.
+
+Git & github tutorial part 1: https://www.youtube.com/watch?v=H0nVBpoTBGc
+Git & github tutorial part 2: https://www.youtube.com/watch?v=gunh_-IkeAw
  */
 package Assignment;
 
