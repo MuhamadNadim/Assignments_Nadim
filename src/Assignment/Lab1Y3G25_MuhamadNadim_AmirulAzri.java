0ef6c7871885b0caef83eecb7fe5c9c9902a4408
Muhamad Nadim <nadimshafii@gmail.com>
2020-06-02 20:39:37 +0800

    Laid out major developments on I/O and methods criteria.

--- src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
+++ src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
@@ -7,6 +7,8 @@ User can reset the value back to zero just like normal calculator. Loop the oper
 
 Git & github tutorial part 1: https://www.youtube.com/watch?v=H0nVBpoTBGc
 Git & github tutorial part 2: https://www.youtube.com/watch?v=gunh_-IkeAw
+
+Refer to notes and videos of Chapters 1-4 for references.
  */
 package Assignment;
 
@@ -19,12 +21,6 @@ public class Lab1Y3G25_MuhamadNadim_AmirulAzri {
 
         System.out.println("This calculator only inputs two values and has six calculation method.");
 
-        System.out.println("Enter the first value: ");
-        int n1 = read.nextInt();
-
-        System.out.println("Enter the second value: ");
-        int n2 = read.nextInt();
-
         System.out.println("Which of kind of calculation you want to see? :- ");
         System.out.println("1. Addition");
         System.out.println("2. Subtraction");
@@ -33,5 +29,48 @@ public class Lab1Y3G25_MuhamadNadim_AmirulAzri {
         System.out.println("5. Power[the first value ^ the second value]");
         System.out.println("6. Square[the first/second value ^ 2]");
 
+        int met = read.nextInt();
+
+        // if(met == 1 || met == 2 || met == 3 || met == 4 || met == 5 || met == 6)
+
+        if(met == 1 || met == 2 || met == 3 || met == 4 || met == 5 || met == 6)
+        {
+
+        }
+
+        System.out.println("Enter the first value: ");
+        int n1 = read.nextInt();
+
+        System.out.println("Enter the second value: ");
+        int n2 = read.nextInt();
+    }
+    static int add(int n1, int n2)
+    {
+        return n1+n2;
+    }
+
+    static int sub(int n1, int n2)
+    {
+        return n1-n2;
+    }
+
+    static int mul(int n1, int n2)
+    {
+        return n1*n2;
+    }
+
+    static int div(int n1, int n2)
+    {
+        return n1/n2;
+    }
+
+    static int pow(int n1, int n2)
+    {
+        return n1^n2;
+    }
+
+    static int sq(int n3)
+    {
+        return (int) Math.pow(n3, 2);
     }
 }
