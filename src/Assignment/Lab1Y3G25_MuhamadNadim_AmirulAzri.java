d72c4fe1b5e35a1b97b65b144176af0071ad7809
Muhamad Nadim <nadimshafii@gmail.com>
2020-06-07 16:07:02 +0800

    Removed the comment at the very top.

--- src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
+++ src/Assignment/Lab1Y3G25_MuhamadNadim_AmirulAzri.java
@@ -1,10 +1,3 @@
-/*
-In a group of two members, design and develop a Java program that performs simple calculation.
-Your program will read in values and operation to be calculated from the user and display the result.
-Your program is able to continue the calculation using the result from previous calculation plus a new value and new operation given by the user.
-Allow the user the select the operation of addition, subtraction, and multiplication, division, power (x^y), and square (x^2).
-User can reset the value back to zero just like normal calculator. Loop the operations until user chooses to quit the program.
- */
 package Assignment;
 
 import java.util.Scanner;
